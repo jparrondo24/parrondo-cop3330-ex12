89fc4049f56c2e21625ebf5ebc2156819d904382
Justin Parrondo <justinparrondo@gmail.com>
2021-06-06 01:35:14 -0400

    Initial commit

---
+++ src/main/java/org/cop3330/App.java
@@ -0,0 +1,27 @@
+package org.cop3330;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter the principal: ");
+        double principle = in.nextDouble();
+        in.nextLine();
+        System.out.print("Enter the rate of interest: ");
+        String strInterest = in.nextLine();
+        double interestRate = Double.parseDouble(strInterest) / 100;
+        System.out.print("Enter the number of years: ");
+        String strYears = in.nextLine();
+        double years = Double.parseDouble(strYears);
+
+        double investment = principle * (1 + interestRate * years);
+        DecimalFormat answerFormat = new DecimalFormat("#.##");
+        String strInvestment = answerFormat.format(investment);
+
+        System.out.printf("After %s years at %s%%, the investment will be worth $%s.\n",
+                           strYears, strInterest, strInvestment);
+    }
+}
